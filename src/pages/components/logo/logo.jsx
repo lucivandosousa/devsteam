96566daad67d131599bef1ebcddf60952a43a8d4
lucivando.sousa <lucivando.sousa@pravaler.com.br>
2023-07-12 13:01:20 -0300

    alterar o alias do componente de imagem

--- src/pages/components/logo/logo.jsx
+++ src/pages/components/logo/logo.jsx
@@ -1,10 +1,10 @@
 import styles from './logo.module.css'
-import ImageLogo from 'next/image' 
+import Image from 'next/image' 
 
 export default function Logo() {
   return (
     <div className={styles.logo}>
-      <ImageLogo src='/logo.svg' width={60} height={60} />
+      <Image src='/logo.svg' width={60} height={60} />
       <h1>DevSteam</h1>
     </div>
   )
